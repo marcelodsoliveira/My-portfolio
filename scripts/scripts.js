db56877a028309465d26840ca23fd244bd0c0a95
Marcelo dos Santos Oliveira <oliveira.lnx@gmail.com>
2021-12-14 14:37:23 -0300

    Upload files
    I just did upload of files to github respository

---
+++ scripts/scripts.js
@@ -0,0 +1,40 @@
+ // setTimeout serve para carregar primeiro as imagens
+  setTimeout(function() {
+    $('#mainBanner').parallax({imageSrc: 'images/banner4.jpg'});
+  }, 200);
+
+  // Scroll das seções
+
+  let navBtn = $('.nav-item');
+  let bannerSection = $('#mainBanner');
+  let aboutSection = $('#about-area');
+  let educSection = $('#my-education');
+  let xpSection = $('#my-experience');
+  let devSection = $('#programming-area');
+  let portfolioSection = $('#projects-area');
+
+  let scrollTo = '';
+
+  $(navBtn).click(function() {
+
+    let btnId = $(this).attr('id');
+
+    if(btnId == 'about-menu') {
+      scrollTo = aboutSection;
+    } else if (btnId == 'education-menu') {
+      scrollTo = educSection;    
+    } else if (btnId == 'experience-menu') {
+      scrollTo = xpSection;
+    } else if (btnId == 'portfolio-menu') {
+      scrollTo = devSection;
+    } else if (btnId == 'projects-area') {
+      scrollTo = portfolioSection;
+    } else {
+      scrollTo = bannerSection;
+    }
+
+    $([document.documentElement, document.body]).animate({
+      scrollTop: $(scrollTo).offset().top - 70
+    }, 1500);
+
+  });
